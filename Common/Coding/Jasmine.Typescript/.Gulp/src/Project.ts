0e81224512a532998464450bf8442274a695d49e
jasmine <jasmine@9a0bef52-b8fb-4613-b061-dbe98fd0deab>
2019-10-03 03:28:26 +0000

    增加项目引用，自动加入编译队列；增加调试配置文件

--- Common/Coding/Jasmine.Typescript/.Gulp/src/Project.ts
+++ Common/Coding/Jasmine.Typescript/.Gulp/src/Project.ts
@@ -15,15 +15,23 @@ export interface IProjectManager
 {
     GetParents( project: string ): Array< string >;
     GetChildren( project: string ): Array< string >;
-    GetProjects(): Array< string >;
+    GetProjects(): ReadonlyArray< ProjectNode >;
 };
 
+
+type UProjectInfo =
+{
+    children: Array< string >;
+    parents: Array< string >;
+};
 
 
 export class CProjectManager implements IProjectManager
 {
     private readonly m_env: IEnvs = null;
     private readonly m_projectNodes: Array< ProjectNode > = Array();
+
+    private readonly m_projectInfos: { [ project: string ]: UProjectInfo } = {};
 
     public constructor( env: IEnvs )
     {
@@ -32,14 +40,16 @@ export class CProjectManager implements IProjectManager
 
     public InitProjects(): this
     {
-        let projects: ProjectNode[] = [];
         for ( const projectName of this.m_env.CompileOptions.Projects )
         {
             this.AddProject( projectName );
         }
 
-        if ( projects.length == 0 )
-            throw new Error();
+        if ( this.m_projectNodes.length )
+            throw new Error( "No projects!" )
+
+        console.log( this.m_projectNodes );
+        console.log( this.m_projectInfos );
 
         return this;
     }
@@ -54,25 +64,49 @@ export class CProjectManager implements IProjectManager
             name: projectName,
             tsProject: ts.createProject( tsconfig )
         };
-        if ( projectNode.tsProject.projectReferences.length > 0 )
+        let referenceNames: Array< string > = Array();;
+        if ( projectNode.tsProject.projectReferences != null &&
+            projectNode.tsProject.projectReferences.length > 0 )
         {
             for ( const reference of projectNode.tsProject.projectReferences )
             {
-                let refName = reference.path.substr( 2 );
+                let refName = path.basename( reference.path );
+                referenceNames.push( refName );
                 this.AddProject( refName );
             }
         }
-        this.m_projectNodes.push( projectNode );
+        if ( this.m_projectNodes.firstOrDefault( n => n.name == projectName ) == null )
+            this.m_projectNodes.push( projectNode );
+
+        this.UpdateProjectInfo( projectName, referenceNames );
     }
 
     public GetParents(project: string): string[] {
         throw new Error("Method not implemented.");
     }
-    public GetChildren(project: string): string[] {
+    public GetChildren(project: string): string[]
+    {
         throw new Error("Method not implemented.");
     }
-    public GetProjects(): string[] {
-        throw new Error("Method not implemented.");
+    public GetProjects(): ReadonlyArray< ProjectNode >
+    {
+        return this.m_projectNodes;
+    }
+
+    private UpdateProjectInfo( project: string, references: Array< string > ): void
+    {
+        for ( const reference of references )
+        {
+            let info = this.m_projectInfos[ reference ];
+            if ( info == null )
+            {
+                info = this.m_projectInfos[ reference ] = {
+                    children: Array(),
+                    parents: Array(),
+                };
+            }
+            info.children.push( project );
+        }
     }
 
     private ScanProjects( dirWorkspace: string, projectNames: ReadonlyArray< string > ): ProjectNode[]
