ec46bd126cf8883d98dc26c4e6a28e2c52d148f7
jasmine <jasmine@9a0bef52-b8fb-4613-b061-dbe98fd0deab>
2019-10-03 15:18:51 +0000

    拓展Array

--- System/JsArrayExtension.d.ts
+++ System/JsArrayExtension.d.ts
@@ -3,11 +3,19 @@ declare interface Array<T>
 {
     firstOrDefault( predicate?: ( e: T, index: number ) => boolean ): T;
     where( predicate: ( e: T, index: number ) => boolean ): Array<T>;
+    RemoveAt( index: number ): Array<T>;
+    Remove<T>( element: T ): Array<T>;
+    RemoveWhere<T>( predicate: ( e: T, index: number ) => boolean ): Array<T>;
 }
 
 declare interface ReadonlyArray<T>
 {
     firstOrDefault( predicate?: ( e: T, index: number ) => boolean ): T;
     where( predicate: ( e: T, index: number ) => boolean ): Array<T>;
+}
+
+declare interface ArrayConstructor
+{
+    convertAll< TInput, TOutput >( array: Array< TInput >, converter: ( input: TInput ) => TOutput ): Array< TOutput >;
 }
 
--- System/JsArrayExtension.ts
+++ System/JsArrayExtension.ts
@@ -4,12 +4,20 @@ declare interface Array<T>
 {
     firstOrDefault( predicate?: ( e: T, index: number ) => boolean ): T;
     where( predicate: ( e: T, index: number ) => boolean ): Array<T>;
+    RemoveAt( index: number ): Array<T>;
+    Remove<T>( element: T ): Array<T>;
+    RemoveWhere<T>( predicate: ( e: T, index: number ) => boolean ): Array<T>;
 }
 
 declare interface ReadonlyArray<T>
 {
     firstOrDefault( predicate?: ( e: T, index: number ) => boolean ): T;
     where( predicate: ( e: T, index: number ) => boolean ): Array<T>;
+}
+
+interface ArrayConstructor
+{
+    convertAll< TInput, TOutput >( array: Array< TInput >, converter: ( input: TInput ) => TOutput ): Array< TOutput >;
 }
 
 Array.prototype.firstOrDefault = function<T>( predicate?: ( e: T, index: number ) => boolean ): T
@@ -37,5 +45,36 @@ Array.prototype.where = function<T>( predicate: ( e: T, index: number ) => boole
             temp.push( this[ i ] );
     }
     return temp;
+}
+
+Array.prototype.RemoveAt = function( this: Array<any>, index: number ): Array<any>
+{
+    return this.splice( index, 1 );
+}
+
+Array.prototype.Remove = function<T>( this: Array<T>, element: T ): Array<T>
+{
+    return this.where( e => e != element );
+}
+
+Array.prototype.RemoveWhere = function<T>( this: Array<T>, predicate: ( e: T, index: number ) => boolean ): Array< T >
+{
+    let temp = [];
+    for ( let i = 0; i < this.length; i ++ )
+    {
+        if ( predicate( this[ i ], i ) == false )
+            temp.push( this[ i ] );
+    }
+    return temp;
+}
+
+Array.convertAll = function< TInput, TOutput >( array: Array< TInput >, converter: ( input: TInput ) => TOutput ): Array< TOutput >
+{
+    let arrayOutput: Array< TOutput > = Array();
+    for ( const a of array )
+    {
+        arrayOutput.push( converter( a ) );
+    }
+    return arrayOutput;
 }
 
